4e96dfe6140cdc026552b6259ba221b263bdfce4
yashasvisxena <yashasvi1802@gmail.com>
2026-02-02 00:16:22 +0530

    minor fixes

--- src/constants/widgets.ts
+++ src/constants/widgets.ts
@@ -1,14 +1,4 @@
-import { TWidgetSize } from '@/types/widget.types';
 import { ChartBar, Sheet, Table } from 'lucide-react';
-
-export const sizeClasses: Record<TWidgetSize, string> = {
-  small: 'col-span-1 row-span-1',
-  medium: 'col-span-1 md:col-span-1 row-span-1',
-  large: 'col-span-1 md:col-span-2 row-span-2',
-  wide: 'col-span-1 md:col-span-2 xl:col-span-3 row-span-1',
-  tall: 'col-span-1 row-span-2',
-  full: 'col-span-1 md:col-span-2 xl:col-span-3 row-span-2',
-};
 
 export const ICON_MAP: Record<
   string,
--- src/types/widget.types.ts
+++ src/types/widget.types.ts
@@ -2,14 +2,6 @@ import { IWidgetApiConfig } from '@/services/api/core/api.types';
 
 import { IWidgetDataMapping } from './mapping.types';
 
-export type TWidgetSize =
-  | 'small'
-  | 'medium'
-  | 'large'
-  | 'wide'
-  | 'tall'
-  | 'full';
-
 export type TWidgetType = 'table' | 'chart' | 'card';
 
 export interface IWidget {
